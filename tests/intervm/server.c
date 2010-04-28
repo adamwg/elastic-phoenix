099d874475a06b4694681a3ee60ef78600b0212a
Cam Macdonell <cam@cs.ualberta.ca>
2010-04-28 15:18:07 -0600

    Swap the names of client and server to make them more logical

--- tests/intervm/server.c
+++ tests/intervm/server.c
@@ -1,84 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include <string.h>
 #include <openssl/sha.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/select.h>
 #include <errno.h>
-#include <time.h>
-#include <sys/ioctl.h>
 #include "ivshmem.h"
 #include "synch.h"
 
-#define CHUNK_SZ (4*1024*1024)
-#define MSI_VECTOR 0 /* the default MSI vector */
+#define CHUNK_SZ (1024l*1024l*4l)
+#define MSI_VECTOR 0 /* the default vector */
+
+int do_select(int fd);
 
 int main(int argc, char ** argv){
 
-    int fd, length=4*1024;
+    long num_chunks, length;
     void * memptr, *regptr;
-    long * long_array;
+    int fd, rv;
+    int i, j, k;
     long param;
     int other;
-    int i, j, k;
-    int count;
     mr_lock_t per_thread;
     mr_lock_t parent;
 
     if (argc != 4){
-        printf("USAGE: server <filename> <param> <other vm>\n");
+        printf("USAGE: sum <filename> <param> <other vm>\n");
         exit(-1);
     }
 
-    fd = open(argv[1], O_RDWR|O_NONBLOCK);
-    printf("[DUMP] opening file %s\n", argv[1]);
-
+    fd = open(argv[1], O_RDWR);
+    printf("[SUM] opening file %s\n", argv[1]);
     param = atol(argv[2]);
     other = atoi(argv[3]);
 
-    length = CHUNK_SZ;
-    printf("[DUMP] size is %d\n", length);
+    printf("[SUM] length is %ld\n", length);
 
-    if ((regptr = mmap(NULL, 256, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0 * getpagesize())) == (void *) -1)
-    {
+    if ((regptr = mmap(NULL, 256, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0 * getpagesize())) == (void *) -1){
         printf("mmap failed (0x%p)\n", regptr);
         close (fd);
         exit (-1);
     }
 
-    if ((memptr = mmap(NULL, length, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 1 * getpagesize())) == (void *) -1)
-    {
+    length = CHUNK_SZ;
+    if ((memptr = mmap(NULL, length, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 1 * getpagesize())) == (void *) -1){
         printf("mmap failed (0x%p)\n", memptr);
         close (fd);
         exit (-1);
     }
 
-//    parent = static_lock_alloc(memptr);
-    parent = memptr;
+    parent = static_lock_alloc(memptr);
     per_thread = static_lock_alloc_per_thread(memptr, ivshmem_get_posn(regptr), parent);
 
-    srand(time(NULL));
-
-    printf("acquiring\n");
     lock_acquire(per_thread);
-    lock_release(per_thread);
+    printf("sleeping for 10 seconds");
+    for (k = 0; k < 10; k++) {
+        sleep(1);
+        printf(".");
+    }
     printf("releasing\n");
+    lock_release(per_thread);
 
-    /* wake client */
-    ivshmem_send(regptr, MSI_VECTOR, other);
-    /*
     printf("waiting\n");
     rv = ivshmem_recv(fd);
     printf("rv = %d\n", rv);
-    */
 
-    printf("[DUMP] munmap is unmapping %p\n", memptr);
+//    printf("md is *%20s*\n", md);
+
     munmap(memptr, length);
     munmap(regptr, 256);
-
     close(fd);
+
+    printf("[SUM] Exiting...\n");
+}
+
+int do_select (int fd) {
+
+    fd_set readset;
+
+    FD_ZERO(&readset);
+    /* conn socket is in Live_vms at posn 0 */
+    FD_SET(fd, &readset);
+
+    select(fd + 1, &readset, NULL, NULL, NULL);
+
+    return 1;
 
 }
