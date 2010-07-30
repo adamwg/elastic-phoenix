7a371c77b176e4e2e36dfd54ae0e4906a5622d67
Adam Wolfe Gordon <adam.wolfegordon@gmail.com>
2010-07-30 12:15:58 -0600

    Fix MASTER and WORKER macros

--- src/distributed.h
+++ src/distributed.h
@@ -14,13 +14,18 @@
 /* Where we should map the memory */
 #define SHM_LOC        (void *)(1024L * 1024L * 1024L * 1024L)
 
+#ifndef MASTER_APP
+#ifndef WORKER_APP
+#error "Enable one of MASTER_APP or WORKER_APP"
+#endif /* !WORKER_APP */
+#endif /* !MASTER_APP */
+
 #ifdef MASTER_APP
 #ifdef WORKER_APP
 #error "Enable only one of MASTER_APP or WORKER_APP"
 #endif /* WORKER_APP */
 #define MASTER if(1)
-#else /* !MASTER_APP */
-#define MASTER if(0)
+#define WORKER if(0);
 #endif /* MASTER_APP */
 
 #ifdef WORKER_APP
@@ -28,8 +33,7 @@
 #error "Enable only one of MASTER_APP or WORKER_APP"
 #endif /* MASTER_APP */
 #define WORKER if(1)
-#else /* !WORKER_APP */
-#define WORKER if(0)
+#define MASTER if(0)
 #endif /* WORKER_APP */
 
 typedef struct {
