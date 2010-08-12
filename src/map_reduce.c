af4f3bd0143872bdb178572d9bf9b189a4117212
Adam Wolfe Gordon <adam.wolfegordon@gmail.com>
2010-08-12 16:03:16 -0600

    Freeing with the wrong free

--- src/map_reduce.c
+++ src/map_reduce.c
@@ -1013,7 +1013,7 @@ static bool reduce_worker_do_next_task (
                 vals = curr_key_val->vals;
                 while (vals != NULL) {
                     next = vals->next_val;
-                    shm_free (vals);
+                    mem_free (vals);
                     vals = next;
                 }
             }
