009d59dc40bcd88275abe6e1738f8987265ad70c
Adam Wolfe Gordon <adam.wolfegordon@gmail.com>
2010-08-25 15:12:51 -0600

    Fixed the splitter for word count

--- tests/word_count/sort.c
+++ tests/word_count/sort.c
@@ -91,7 +91,7 @@ static int mr_mypartition(int reduce_tasks, void* key, int key_size)
 static int mr_sort_splitter(void *data_in, int req_units, map_args_t *out, splitter_mem_ops_t *mem) {
 	static int offset = 0;
 
-	if(offset > n_elems * unit_size) {
+	if(offset >= n_elems * unit_size) {
 		return(0);
 	}
 	
