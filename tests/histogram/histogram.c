d9cd069978df5ba093cbb905688395e0876ba3ab
Adam Wolfe Gordon <awg@xvx.ca>
2010-12-16 11:22:19 -0700

    Turn up chunk size for histogram

--- tests/histogram/histogram.c
+++ tests/histogram/histogram.c
@@ -318,7 +318,7 @@ int main(int argc, char *argv[]) {
     map_reduce_args.splitter = hist_splitter;
     map_reduce_args.key_cmp = myshortcmp;
     
-    map_reduce_args.unit_size = 3;  // 3 bytes per pixel
+    map_reduce_args.unit_size = 3072;  // 3 bytes per pixel * 1024 pixels per map
 	hist_data.unit_size = 3;
     map_reduce_args.partition = NULL; // use default
     map_reduce_args.result = &hist_vals;
